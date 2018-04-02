be045a38b59d22affc5b58290f002c6986a0a6e6
VadimGrogul <grogulvadim@gmail.com>
2018-04-02 20:25:57 +0300

    js

---
+++ js/footerAnimation.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $(".genre-row").css("opacity", 0);
+
+    $(window).scroll(function(){
+        var scrollHeight = $(window).scrollTop();
+        var genreRow = $(".genre-row");
+        var count = 0;
+
+        if(scrollHeight > 1000) {
+            setInterval(function(){
+                genreRow.eq(count).animate({"opacity": 1});
+
+                if(count >= genreRow.length){
+                    return false;
+                } else {
+                    count++;
+                }
+            }, 500);
+        };
+    });
+});
